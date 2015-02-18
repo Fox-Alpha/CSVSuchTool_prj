54120fed9e6e05f1dbbdd2df40757150c13cea25
Dirk.Buck <dirk.buck@alpha-com.de>
2015-02-18 14:54:31 +0100

    Add: Form für Kurznotizen Editor

---
+++ ShortNotesForm.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 18.02.2015
+ * Zeit: 14:44
+ * 
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CSVSuchTool
+{
+	/// <summary>
+	/// Description of ShortNotesForm.
+	/// </summary>
+	public partial class ShortNotesForm : Form
+	{
+		public ShortNotesForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
