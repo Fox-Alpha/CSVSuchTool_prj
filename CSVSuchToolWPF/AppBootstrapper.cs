705d9b07c5c7b6f3d76533d51eefedcadcef8f69
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2016-11-11 14:48:21 +0100

    Add: Bootstrapper funktionaltät

---
+++ CSVSuchToolWPF/AppBootstrapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using CSVSuchToolWPF.ViewModels;
+using Stylet;
+using StyletIoC;
+//using CSVSuchToolWPF.Services;
+
+namespace CSVSuchToolWPF
+{
+    public class Bootstrapper : Bootstrapper<MainWindowViewModel>
+    {
+        protected override void OnStart()
+        {
+            // This is called just after the application is started, but before the IoC container is set up.
+            // Set up things like logging, this.Assemblies, etc
+        }
+        
+        protected override void ConfigureIoC(IStyletIoCBuilder builder)
+        {
+            // Bind your own types. Concrete types are automatically self-bound.
+        }
+
+        protected override void Configure()
+        {
+            // This is called after Stylet has created the IoC container, so this.Container exists, but before the
+            // Root ViewModel is launched.
+            // Configure your services, etc, in here
+        }
+
+        protected override void OnLaunch()
+        {
+            // This is called just after the root ViewModel has been launched
+            // Something like a version check that displays a dialog might be launched from here
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Called on Application.Exit
+        }
+
+        protected override void OnUnhandledException(DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Called on Application.DispatcherUnhandledException
+        }
+    }
+}
