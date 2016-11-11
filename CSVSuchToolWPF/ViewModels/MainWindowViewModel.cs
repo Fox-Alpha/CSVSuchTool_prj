748cc4766c0c2ecc4d97af3e5a4b229e66b8824d
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2016-11-11 14:51:35 +0100

    Chg: Umstellung auf MVVM

---
+++ CSVSuchToolWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stylet;
+using StyletIoC;
+
+namespace CSVSuchToolWPF.ViewModels
+{
+    public class MainWindowViewModel : Screen
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
